a4a5aa4b62e8aaf0bcff0ea0660ae45f621f31ce
Clément <clement@test.com>
2026-04-16 14:31:55 +0200

    fix: better lean angle calc (lateral vector)

--- lib/services/lean_tracker.dart
+++ lib/services/lean_tracker.dart
@@ -5,7 +5,15 @@ import 'package:sensors_plus/sensors_plus.dart';
 class LeanTracker {
   double _lean = 0;
   double _rawAngle = 0;
-  double _reference = 0;
+  double _refX = 0;
+  double _refY = 0;
+  double _refZ = 0;
+  double _sideX = 0;
+  double _sideY = 0;
+  double _sideZ = 0;
+  double _lastX = 0;
+  double _lastY = 0;
+  double _lastZ = 0;
   double _maxLeft = 0.0;
   double _maxRight = 0.0;
   double _debugRaw = 0;
@@ -36,17 +44,17 @@ class LeanTracker {
   }
 
   double applyCalibration(double rawAngle) {
-    return rawAngle - _reference;
-  }
+     return rawAngle;
+   }
 
   double computeLean(double x, double y, double z) {
     final rawAngle = computeRawAngle(x, y, z);
     if (rawAngle.isNaN) return double.nan;
 
     final corrected = applyCalibration(rawAngle);
-    _debugRaw = -corrected;
+    _debugRaw = corrected;
 
-    _lean = smooth(_lean, -corrected);
+    _lean = smooth(_lean, corrected);
 
     if (_lean.abs() < 2) {
       _lean = 0;
@@ -64,6 +72,10 @@ class LeanTracker {
 
   void start() {
     _subscription = accelerometerEvents.listen((event) {
+      _lastX = event.x;
+      _lastY = event.y;
+      _lastZ = event.z;
+
       final smoothed = computeLean(event.x, event.y, event.z);
       if (smoothed.isNaN) return;
 
@@ -84,14 +96,43 @@ class LeanTracker {
   }
 
   double computeRawAngle(double x, double y, double z) {
-    final denom = sqrt(y * y + z * z);
-    if (denom < 0.1) return double.nan;
+    final gravityOnSide = (x * _sideX) + (y * _sideY) + (z * _sideZ);
+    final gravityOnUp = (x * _refX) + (y * _refY) + (z * _refZ);
 
-    return atan2(x, denom) * 57.2958;
+    final sideNorm = sqrt((_sideX * _sideX) + (_sideY * _sideY) + (_sideZ * _sideZ));
+    final upNorm = sqrt((_refX * _refX) + (_refY * _refY) + (_refZ * _refZ));
+
+    if (sideNorm < 0.1 || upNorm < 0.1) {
+      return 0;
+    }
+
+    final side = gravityOnSide / sideNorm;
+    final up = gravityOnUp / upNorm;
+
+    return atan2(side, up) * 57.2958;
   }
 
   void calibrate() {
-    _reference = _rawAngle;
+    _refX = _lastX;
+    _refY = _lastY;
+    _refZ = _lastZ;
+    final refNorm = sqrt((_refX * _refX) + (_refY * _refY) + (_refZ * _refZ));
+
+    if (refNorm > 0.1) {
+      final upX = _refX / refNorm;
+      final upY = _refY / refNorm;
+      final upZ = _refZ / refNorm;
+
+      // axe horizontal fixe dans "le monde"
+      const worldForwardX = 0.0;
+      const worldForwardY = 1.0;
+      const worldForwardZ = 0.0;
+
+      _sideX = upY * worldForwardZ - upZ * worldForwardY;
+      _sideY = upZ * worldForwardX - upX * worldForwardZ;
+      _sideZ = upX * worldForwardY - upY * worldForwardX;
+    }
+
     _lean = 0;
     _lastDisplayedLean = 0;
     _lastUpdate = DateTime.now();
